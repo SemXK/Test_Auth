1dcbec4d48de3903c9001d01841c4bdbf47b649b
SemXK <72094409+SemXK@users.noreply.github.com>
2023-08-01 00:17:40 +0200

    Added first API

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,36 @@
 import express from 'express'
+import { Prisma, PrismaClient } from '@prisma/client'
 
 //Fundamental constants
+const prisma = new PrismaClient()
 const server = express()
 const PORT = 4000
+
+
 //HTTP Get Requests
-server.get('/', (req, res) => {
-  res.status(200).send({msg: 'server is on home page !'})
+server.get('/', async (req, res) => {
+  const dashboards = await prisma.dashboard.findMany({  //Store all 'Dashboard' instances in a variable
+    orderBy: {
+      position: 'asc'     //Pretty clear
+    },
+    include: {  //Add contents linked the every instance of 'Dashboard'
+      contents: {
+        orderBy:{
+          position:'desc'
+        }
+      }    
+    }
+  }) 
+  res.status(200).send({msg: dashboards})
 })
 
-//POST listener
-server.listen(PORT, () => {
+//PORT listener
+const serverInstance = server.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`)
+})
+
+process.on('SIGTERM', async () => {           //Launch every time the application is softly rebooted
+  console.log('Server going down...');
+  serverInstance.close();
+  await prisma.$disconnect();      //prevents prisma from locking the disconnection of the application
 })
